9138b379e2d56c3ab61c81cea562769f6dcdcf98
Adil grover <adilgro@gmail.com>
2026-09-15 00:40:12 +0530

    unused var test

--- index.js
+++ index.js
@@ -4,3 +4,4 @@ const app = express();
 app.get('/', (req, res) => res.send('Hello from CI/CD project'));
 
 app.listen(2950, () => console.log('Server running on port 3000'));
+const unusedVar = "this will trigger a lint error";
